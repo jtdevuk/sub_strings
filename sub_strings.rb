0d77adfb6f8f3255f99c56e77e7dea9a0570350d
John Tweeddale <j.tweeddale@outlook.com>
2020-08-18 10:56:21 +0100

    Works for single word in string

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,3 +1,16 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
- 
-puts dictionary
+
+def substrings(string, dictionary)
+  substrings = Hash.new(0)
+  dictionary.each do |word|
+  substrings[word] += 1 if string.include?(word)
+  end
+  puts substrings
+end
+
+
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+
+# try using array1.product(array2)
